9a3ceabd10f0fd927c0634498d5a584f801da63f
madurasinghekldp <madurasinghekldp@gmail.com>
2025-07-17 18:36:32 +0530

    feat: implement product module with CRUD operations and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
@@ -13,10 +15,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: 'dulan',
       database: 'jewelrydb',
       entities: [],
-      synchronize: true,
+      synchronize: true, // Note: set to false in production
     }),
+    ProductModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
